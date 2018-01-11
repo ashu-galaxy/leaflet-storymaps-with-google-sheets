e31dbf046d7739cab19ae4996133932455dd6841
ashu-galaxy <33547185+ashu-galaxy@users.noreply.github.com>
2018-01-11 15:47:18 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1d4_gUTNBRkh90c4zJ_IdRn7v5UiQe6tcUl2uPBJz7Uk/edit#gid=0';
